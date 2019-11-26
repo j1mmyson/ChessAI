6fa6f8a88f75550c904afe6ae5f8974a2da88201
applemango <applemango@applemangoui-iMac.local>
2019-11-26 17:21:42 +0900

    jw-v1

--- random_move_AI.py
+++ random_move_AI.py
@@ -25,6 +25,14 @@ while True:
         turn = chess.WHITE
     else:
         # player
+        legal_list = []
+        for i in board.legal_moves:
+            legal_list.append(str(i))
+
         p_move = input("type your move(just like 'a2a4'):")
-        board.push(chess.Move.from_uci(p_move))
+        while True:
+            if p_move in legal_list:
+                board.push(chess.Move.from_uci(p_move))
+            else:        
+                p_move = input("type your move(just like 'a2a4'):")
         turn = chess.BLACK
