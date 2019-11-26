8d460bdc282bbb3eddafca8c15183a0d96303863
Wook-2 <bwson98@gmail.com>
2019-11-26 11:41:36 +0900

    ver 1.0

---
+++ random_move_AI.py
@@ -0,0 +1,30 @@
+import chess
+import random
+import os
+import time
+
+turn = chess.WHITE
+gameover = False
+board = chess.Board()
+
+
+while True:
+    os.system('clear')
+    print(board)
+    if(board.is_game_over() is True):
+        print("GAME OVER")
+        break
+    if turn is chess.BLACK:
+        # computer(AI) play
+        legal_list = []
+        for i in board.legal_moves:
+            legal_list.append(str(i))
+        c_move = chess.Move.from_uci(random.choice(legal_list))
+        board.push(c_move)
+        time.sleep(1.5)
+        turn = chess.WHITE
+    else:
+        # player
+        p_move = input("type your move(just like 'a2a4'):")
+        board.push(chess.Move.from_uci(p_move))
+        turn = chess.BLACK
