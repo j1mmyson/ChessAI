b70c3a14c315192983ed731f92426a11233ff6c1
Wook-2 <bwson98@gmail.com>
2019-11-26 19:53:36 +0900

    add function "print_board()"

--- random_move_AI.py
+++ random_move_AI.py
@@ -1,7 +1,29 @@
 import chess
-import random
 import os
+import random
 import time
+
+
+
+def print_board(board):
+    symbol = ['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k']
+    j = 7
+    k = 0
+    print("8"+"   ", end='')
+    for i in board.board_fen():
+        print()
+        print(j, "  ", end='')
+        j = j-1
+    elif (i in symbol) is True:
+        print(i, "", end='')
+    else:
+        for k in range(0, int(i)):
+            print("- ", end='')
+        k = 0
+    print()
+    print()
+    print("    "+"a b c d e f g h")
+
 
 turn = chess.WHITE
 gameover = False
@@ -10,7 +32,7 @@ board = chess.Board()
 
 while board.is_game_over() is False:
     os.system('clear')
-    print(board)
+    print_board(board)
     print(board.is_game_over())
     # if board.is_game_over() is True:
     #     print("GAME OVER")
