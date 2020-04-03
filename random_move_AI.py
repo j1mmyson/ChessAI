75a68d9555c4fddf6e37d8925cd002638cd30d52
sugyeonghh <55546817+sugyeonghh@users.noreply.github.com>
2020-04-03 16:29:47 +0900

    사용x

--- random_move_AI.py
+++ random_move_AI.py
@@ -30,21 +30,25 @@ gameover = False
 board = chess.Board()
 
 
-while board.is_game_over() is False:
+while True:
     os.system('clear')
     print_board(board)
-    print(board.is_game_over())
-    # if board.is_game_over() is True:
-    #     print("GAME OVER")
-    #     break
+    
     if turn is chess.BLACK:
         # computer(AI) play
         legal_list = []
         for i in board.legal_moves:
             legal_list.append(str(i))
-        c_move = chess.Move.from_uci(random.choice(legal_list))
-        board.push(c_move)
-        time.sleep(1.5)
+        # c_move = chess.Move.from_uci(random.choice(legal_list))
+        # board.push(c_move)
+        # time.sleep(1.5)
+        c_move = input("type your move(just like 'a2a4'):")
+        while True:
+            if c_move in legal_list:
+                board.push(chess.Move.from_uci(c_move))
+                break
+            else:
+                c_move = input("type your move(just like 'a2a4'):")
         turn = chess.WHITE
     else:
         # player
@@ -61,4 +65,15 @@ while board.is_game_over() is False:
                 p_move = input("type your move(just like 'a2a4'):")
         turn = chess.BLACK
 
-print("byungwook")
+    if board.is_game_over() is True:
+        break
+
+if board.is_game_over() is True:
+    print(board.result())
+    if board.result() == "1-0":
+        print('\nWHITE win\n')
+    elif board.result() == "0-1":
+        print('\nBLACK win\n')
+    elif board.result() == "1/2-1/2":
+        print('\nDraw!\n')
+
