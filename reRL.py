3aab17b5c3a61278982e5fae72f34f0b64eadd1f
sugyeonghh <55546817+sugyeonghh@users.noreply.github.com>
2020-04-07 19:29:17 +0900

    Create reRL.py

---
+++ reRL.py
@@ -0,0 +1,34 @@
+import pickle
+
+
+class LinkedList:
+    class Node:
+        def __init__(self, move, prev):
+            self.move = move
+            self.reward = 0.5
+            self.prev = prev
+            self.next = []
+
+    def __init__(self):
+        self.head = self.Node(None, None)
+        self.size = 0
+
+    def insert(self, move, p):
+        new_node = self.Node(move, p)
+        p.next.append(new_node)
+        self.size += 1
+
+
+with open('data_01.pickle', 'rb') as f:
+    data = pickle.load(f)
+
+current_node = data.head
+
+while(1):
+    print(current_node.reward)
+    current_node = current_node.next[0]
+    if len(current_node.next) == 0:
+        print('size:', data.size)
+        break
+
+
