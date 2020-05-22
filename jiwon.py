2c571f4e68e4d1c9f59b3bb36394ce86a769b10e
Jiwon-Choi <32658880+JiWon-Choi@users.noreply.github.com>
2020-05-22 17:55:20 +0900

    Update jiwon.py

--- jiwon.py
+++ jiwon.py
@@ -1,7 +1,7 @@
 import chess
 import random
-import os
-import time
+#import os
+#import time
 import pickle
 import sys
 
@@ -10,7 +10,7 @@ BOLD = '\033[1m'
 CEND = '\033[0m'
 CRED = '\033[31m'
 CGRAY = '\033[90m'
-REPEAT = 250000
+REPEAT = 100
 
 class LinkedList:
     class Node:
@@ -26,8 +26,8 @@ class LinkedList:
         self.size = 0
         self.accumulated_board = 0
 
-    def insert(self, move, p):
-        new_node = self.Node(move, p, None)
+    def insert(self, move, p, state):
+        new_node = self.Node(move, p, state)
         p.next.append(new_node)
         self.size += 1
 
@@ -38,6 +38,7 @@ def pre_order(node, new_state, current_node):
     else:
         for i in node.next:
             pre_order(i, new_state, current_node)
+    return 0
 
 # display
 def display():
@@ -50,26 +51,6 @@ def display():
         else:
             print(CGRAY+BOLD+reboard[i]+CEND, end='')
     print()
-
-# 잡은 말 count
-def captured_count(before_board, after_board):
-    before_list=[0]*12
-    after_list=[0]*12
-
-    for i in range(len(before_board)):
-        for j in range(len(piece_list)):
-            if piece_list[j] is before_board[i]:
-                before_list[j] += 1
-
-    for i in range(len(after_board)):
-        for j in range(len(piece_list)):
-            if piece_list[j] is after_board[i]:
-                after_list[j] += 1
-
-    for i in range(len(before_list)):
-        if before_list[i] > after_list[i]:
-            if (sum(before_list) > sum(after_list)):
-                piece[i][1]+=1
 
 
 try:
@@ -102,40 +83,45 @@ for i in range(REPEAT):
             legal_list.append(str(i))
 
         if 0.99999**chess_model.accumulated_board >= 0.2:
-            epsilon = 0.99999**chess_model.accumulated_board # epsilon의 확률로 랜덤, 0.9999^x 곡선으로 epsilon 변화
+            epsilon = 0.99999**chess_model.accumulated_board # epsilon의 확률로 랜덤, 0.99999^x 곡선으로 epsilon 변화
         else:
             epsilon = 0.2
 
         # epsilon의 확률로 랜덤
         random_value = random.random()
         
-        if epsilon <= random_value: # 최선의 수 선택
-            if len(current_node.next) == 0:  # next가 비어있는 경우 랜덤
-                random_move = random.choice(legal_list)
-                selected_move = chess.Move.from_uci(random_move)
-                board.push(selected_move)
+        if not current_node.next: # next가 비어있는 경우 랜덤
+        #if len(current_node.next) == 0:  
+            random_move = random.choice(legal_list)
+            selected_move = chess.Move.from_uci(random_move)
+            board.push(selected_move)
                 
-                state = board
-                search_result = pre_order(chess_model.head, state, current_node)
+            state = board
+            search_result = pre_order(chess_model.head, state, current_node)
 
-                if search_result is 100:
-                    current_node = current_node.next[-1]
-                else:
-                    chess_model.insert(random_move, current_node)
-                    current_node.state = board
-                    current_node = current_node.next[-1]
-
+            if search_result is 100:
+                current_node = current_node.next[-1]
             else:
-                next_node = current_node.next[0]
-                for i in current_node.next:
-                    if next_node.reward < i.reward:
-                        next_node = i
-                current_node = next_node
-                selected_move = chess.Move.from_uci(current_node.move)
+                chess_model.insert(random_move, current_node, state)
+                current_node = current_node.next[-1]
+
+        elif epsilon <= random_value: # 최선의 수 선택
+            current_node = current_node[0]
+            selected_move = chess.Move.from_uci(current_node.move)
+            board.push(selected_move)
 
         else: # 탐험
-            if len(current_node.next) == 0:  # next가 비어있는 경우 랜덤
-                random_move = random.choice(legal_list)
+            random_move = random.choice(legal_list)
+            find = False
+            for i in current_node.next:
+                if random_move == i.move:
+                    find = True
+                    break
+            if find is True:
+                current_node = i
+                selected_move = chess.Move.from_uci(current_node.move)
+                board.push(selected_move)
+            else:
                 selected_move = chess.Move.from_uci(random_move)
                 board.push(selected_move)
                 
@@ -145,26 +131,10 @@ for i in range(REPEAT):
                 if search_result is 100:
                     current_node = current_node.next[-1]
                 else:
-                    chess_model.insert(random_move, current_node)
-                    current_node.state = board
+                    chess_model.insert(random_move, current_node, state)
                     current_node = current_node.next[-1]
-            else:
-                random_move = random.choice(legal_list)
-                find = 0
-                for i in current_node.next:
-                    if random_move == i.move:
-                        find = 1
-                        current_node = i
-                        break
-                if find == 0:
-                    chess_model.insert(random_move, current_node)
-                    current_node = current_node.next[-1]
-                selected_move = chess.Move.from_uci(current_node.move)
 
-        before_board = str(board)
         floor += 1
-        after_board = str(board)
-        captured_count(before_board, after_board)
 
         if board.is_game_over() is True:
             break
@@ -177,10 +147,25 @@ for i in range(REPEAT):
         winning_point = 1
 
     while True:
+        before_reward = 0
         if my_floor%2 == floor%2:
+            before_reward = current_node.reward
             current_node.reward = current_node.reward*2/3 + winning_point*(my_floor/floor)/3
         else:
+            before_reward = current_node.reward
             current_node.reward = current_node.reward*2/3 + (1-winning_point)*(my_floor/floor)/3
+
+        if current_node.prev.next[0] == current_node and before_reward-current_node.reward > 0:
+            max_reward = current_node.reward
+            for i in current_node.prev.next:
+                if max_reward < i.reward:
+                    tmp = i
+                    i = current_node
+                    current_node = tmp
+        elif current_node.reward > current_node.prev.next[0].reward and before_reward-current_node.reward < 0:
+            tmp = current_node
+            current_node = current_node.prev.next[0]
+            current_node.prev.next[0] = tmp
 
         my_floor -= 1
         current_node = current_node.prev
