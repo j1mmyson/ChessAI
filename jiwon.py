05065fdd306d8bb01d3c4c03d7491282ffd6fe08
Jiwon-Choi <32658880+JiWon-Choi@users.noreply.github.com>
2020-05-22 19:56:37 +0900

    Update jiwon.py

--- jiwon.py
+++ jiwon.py
@@ -1,16 +1,11 @@
 import chess
 import random
-#import os
-#import time
+import os
+import time
 import pickle
 import sys
 
-
-BOLD = '\033[1m'
-CEND = '\033[0m'
-CRED = '\033[31m'
-CGRAY = '\033[90m'
-REPEAT = 100
+REPEAT = 5
 
 class LinkedList:
     class Node:
@@ -37,21 +32,10 @@ def pre_order(node, new_state, current_node):
         return 100
     else:
         for i in node.next:
-            pre_order(i, new_state, current_node)
+            result = pre_order(i, new_state, current_node)
+            if result == 100:
+                break
     return 0
-
-# display
-def display():
-    reboard = str(board)
-    for i in range(len(reboard)):
-        if reboard[i].islower() is True:
-            print(CRED+BOLD+reboard[i]+CEND, end='')
-        elif reboard[i].islower() is False:
-            print(reboard[i], end='')
-        else:
-            print(CGRAY+BOLD+reboard[i]+CEND, end='')
-    print()
-
 
 try:
     data = open('data.pickle', 'rb')
@@ -63,14 +47,8 @@ else:
         data = pickle.load(f)
     chess_model = data
 
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(1000000)
 current_node = chess_model.head
-piece_list = ['p','r','n','b','q','k','P','R','N','B','Q','K']
-piece = []
-for i in range(12):
-    line = [0, 0]
-    piece.append(line)
-    piece[i][0]= piece_list[i]
 
 # main
 for i in range(REPEAT):
@@ -88,10 +66,10 @@ for i in range(REPEAT):
             epsilon = 0.2
 
         # epsilon의 확률로 랜덤
+        #random_value = 0
         random_value = random.random()
         
         if not current_node.next: # next가 비어있는 경우 랜덤
-        #if len(current_node.next) == 0:  
             random_move = random.choice(legal_list)
             selected_move = chess.Move.from_uci(random_move)
             board.push(selected_move)
@@ -99,26 +77,27 @@ for i in range(REPEAT):
             state = board
             search_result = pre_order(chess_model.head, state, current_node)
 
-            if search_result is 100:
+            if search_result == 100:
                 current_node = current_node.next[-1]
             else:
                 chess_model.insert(random_move, current_node, state)
                 current_node = current_node.next[-1]
 
         elif epsilon <= random_value: # 최선의 수 선택
-            current_node = current_node[0]
+            current_node = current_node.next[0]
             selected_move = chess.Move.from_uci(current_node.move)
             board.push(selected_move)
 
         else: # 탐험
             random_move = random.choice(legal_list)
             find = False
+
             for i in current_node.next:
                 if random_move == i.move:
                     find = True
+                    current_node = i
                     break
             if find is True:
-                current_node = i
                 selected_move = chess.Move.from_uci(current_node.move)
                 board.push(selected_move)
             else:
@@ -128,7 +107,7 @@ for i in range(REPEAT):
                 state = board
                 search_result = pre_order(chess_model.head, state, current_node)
 
-                if search_result is 100:
+                if search_result == 100:
                     current_node = current_node.next[-1]
                 else:
                     chess_model.insert(random_move, current_node, state)
@@ -146,26 +125,26 @@ for i in range(REPEAT):
     else:
         winning_point = 1
 
-    while True:
-        before_reward = 0
-        if my_floor%2 == floor%2:
-            before_reward = current_node.reward
-            current_node.reward = current_node.reward*2/3 + winning_point*(my_floor/floor)/3
-        else:
-            before_reward = current_node.reward
-            current_node.reward = current_node.reward*2/3 + (1-winning_point)*(my_floor/floor)/3
+    # while True:
+    #     before_reward = 0
+    #     if my_floor%2 == floor%2:
+    #         before_reward = current_node.reward
+    #         current_node.reward = current_node.reward*2/3 + winning_point*(my_floor/floor)/3
+    #     else:
+    #         before_reward = current_node.reward
+    #         current_node.reward = current_node.reward*2/3 + (1-winning_point)*(my_floor/floor)/3
 
-        if current_node.prev.next[0] == current_node and before_reward-current_node.reward > 0:
-            max_reward = current_node.reward
-            for i in current_node.prev.next:
-                if max_reward < i.reward:
-                    tmp = i
-                    i = current_node
-                    current_node = tmp
-        elif current_node.reward > current_node.prev.next[0].reward and before_reward-current_node.reward < 0:
-            tmp = current_node
-            current_node = current_node.prev.next[0]
-            current_node.prev.next[0] = tmp
+    #     if current_node.prev.next[0] == current_node and before_reward-current_node.reward > 0:
+    #         max_reward = current_node.reward
+    #         for i in current_node.prev.next:
+    #             if max_reward < i.reward:
+    #                 tmp = i
+    #                 i = current_node
+    #                 current_node = tmp
+    #     elif current_node.reward > current_node.prev.next[0].reward and before_reward-current_node.reward < 0:
+    #         tmp = current_node
+    #         current_node = current_node.prev.next[0]
+    #         current_node.prev.next[0] = tmp
 
         my_floor -= 1
         current_node = current_node.prev
@@ -182,3 +161,5 @@ for i in range(REPEAT):
 
 with open('data.pickle', 'wb') as f:
     pickle.dump(chess_model, f, pickle.HIGHEST_PROTOCOL)
+
+print("성공!")
